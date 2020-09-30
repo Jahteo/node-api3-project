5257f94d74aacf839cafcbc0d657b60838e16cae
Josh Glantz-Hucks <jahteo@gmail.com>
2020-09-30 13:24:53 -0700

    first GET working, yay

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // code away!
 const server = require('./server.js');
 
+const port = 4000;
 server.listen(4000, () => {
   console.log('\n* Server Running on http://localhost:4000 *\n')
 })
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const usersRouter = require('./users/userRouter')
 
 const server = express();
+server.use(express.json());
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
+
+//endpoints
+server.use("/api/users", usersRouter);
 
 //custom middleware
 
